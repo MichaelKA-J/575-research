c99124ac6e4f58afd7f425c242d8ae891e0e5811
MichaelKA-J <98190608+MichaelKA-J@users.noreply.github.com>
2026-03-30 20:54:22 -0400

    Update data_acquisition.py

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 20:35:53 2026
 
+@author: michael
+"""
+
+import lseg.data as ld
+
+# obtaining raw oil and gas future prices via API
+ld.open_session()
+prices_df = ld.get_history(
+    universe=["LCOc1", "NGc1"],
+    fields=["SETTLE"],
+    count=120,
+    interval="daily"
+)
+ld.close_session()
+
+# data cleaning
+prices_df = prices_df[-100:]
+
+# export to csv
+prices_df.to_csv("raw_energy_prices.csv")
